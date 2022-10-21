dcaf7d9da1dd2bbf405b43d8318a4bce0bdac30a
Jacob Matson <matsonj@gmail.com>
2022-10-21 04:46:45 +0000

    knit in 538 schedule

--- transform/models/conformed/inputs/schedules.sql
+++ transform/models/conformed/inputs/schedules.sql
@@ -4,31 +4,9 @@
 ) }}
 
 SELECT
-    S.key::int AS game_id,
-    S.type,
-    S.series_id,
-    V.conf AS visiting_conf,
-    V.team AS visiting_team,
-    V.elo_rating::int AS visiting_team_elo_rating,
-    H.conf AS home_conf,
-    H.team AS home_team,
-    H.elo_rating::int AS home_team_elo_rating
-FROM {{ ref( 'prep_schedule' ) }} AS S
-LEFT JOIN {{ ref( 'prep_team_ratings' ) }} V ON V.team_long = S.visitorneutral
-LEFT JOIN {{ ref( 'prep_team_ratings' ) }} H ON H.team_long = S.homeneutral
-WHERE S.type = 'reg_season'
-GROUP BY ALL
+    *
+FROM {{ ref( 'reg_season_schedule' ) }}
 UNION ALL
 SELECT
-    S.key::int AS game_id,
-    S.type,
-    S.series_id,
-    NULL AS visiting_conf,
-    S.visitorneutral AS visiting_team,
-    NULL AS visiting_team_elo_rating,
-    NULL AS home_conf,
-    S.homeneutral AS home_team,
-    NULL AS home_team_elo_rating
-FROM {{ ref( 'prep_schedule' ) }} AS S
-WHERE S.type <> 'reg_season'
-GROUP BY ALL
+    *
+FROM {{ ref( 'post_season_schedule' ) }}
