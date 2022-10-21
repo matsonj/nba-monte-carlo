352fe8b0b186da2e7b34a244478fa414b28adefc
Jacob Matson <matsonj@gmail.com>
2022-10-21 04:46:45 +0000

    knit in 538 schedule

---
+++ transform/models/conformed/inputs/reg_season_schedule.sql
@@ -0,0 +1,19 @@
+{{
+    config(
+        materialized = "ephemeral" if target.name == 'parquet' else "view"
+) }}
+
+SELECT
+    (S._smart_source_lineno - 1)::int AS game_id,
+    'regular_season' AS type,
+    0 as series_id,
+    V.conf AS visiting_conf,
+    V.team AS visiting_team,
+    V.elo_rating::int AS visiting_team_elo_rating,
+    H.conf AS home_conf,
+    H.team AS home_team,
+    H.elo_rating::int AS home_team_elo_rating
+FROM {{ ref( 'prep_nba_elo_latest' ) }} AS S
+LEFT JOIN {{ ref( 'prep_team_ratings' ) }} V ON V.team = S.team2
+LEFT JOIN {{ ref( 'prep_team_ratings' ) }} H ON H.team = S.team1
+GROUP BY ALL
