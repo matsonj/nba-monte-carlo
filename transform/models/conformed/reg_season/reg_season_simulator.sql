ccf8bfbabab42a4316163f0892a1de4622306bcd
Jacob Matson <matsonj@gmail.com>
2022-10-21 06:08:31 +0000

    handling actuals

--- transform/models/conformed/reg_season/reg_season_simulator.sql
+++ transform/models/conformed/reg_season/reg_season_simulator.sql
@@ -11,11 +11,11 @@ SELECT
     {{ elo_calc( 'S.home_team_elo_rating', 'S.visiting_team_elo_rating' ) }} as home_team_win_probability,
     R.rand_result,
     CASE 
-        WHEN {{ var('include_actuals') }} THEN LR.winning_team
+        WHEN LR.include_actuals = true THEN LR.winning_team
         WHEN {{ elo_calc( 'S.home_team_elo_rating', 'S.visiting_team_elo_rating' ) }}  >= R.rand_result THEN S.home_team
         ELSE S.visiting_team
     END AS winning_team,
-    {{ var('include_actuals') }} AS include_actuals
+    COALESCE(LR.include_actuals, false) AS include_actuals
 FROM {{ ref( 'schedules' ) }} S
 LEFT JOIN {{ "'/tmp/storage/random_num_gen.parquet'" if target.name == 'parquet'
         else ref( 'random_num_gen' ) }} R ON R.game_id = S.game_id
