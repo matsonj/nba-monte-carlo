967a9cb6f56dd98efd3455c7695fc39e093fd8bf
Jacob Matson <matsonj@gmail.com>
2022-10-23 01:26:17 +0000

    knitting in changing ELO to match actuals

--- transform/models/conformed/inputs/ratings.sql
+++ transform/models/conformed/inputs/ratings.sql
@@ -4,9 +4,15 @@
 ) }}
 
 SELECT
-    team,
-    team_long,
-    conf,
-    elo_rating::int AS elo_rating
-FROM {{ ref( 'prep_team_ratings' ) }}
+    orig.team,
+    orig.team_long,
+    orig.conf,
+    CASE
+        WHEN latest.latest_ratings = true THEN latest.elo_rating::int
+        ELSE orig.elo_rating::int
+    END AS elo_rating,
+    orig.elo_rating::int AS original_rating,
+    orig.win_total
+FROM {{ ref( 'prep_team_ratings' ) }} orig
+LEFT JOIN {{ ref( 'prep_latest_ratings' )}} latest ON latest.team = orig.team
 GROUP BY ALL
