424a7ce3620e940ad5f398d6bbd5a6973eb5c3b1
Jacob Matson <matsonj@gmail.com>
2022-10-23 19:04:35 +0000

    adding elo updates

--- transform/models/conformed/inputs/ratings.sql
+++ transform/models/conformed/inputs/ratings.sql
@@ -8,11 +8,11 @@ SELECT
     orig.team_long,
     orig.conf,
     CASE
-        WHEN latest.latest_ratings = true THEN latest.elo_rating::int
-        ELSE orig.elo_rating::int
+        WHEN latest.latest_ratings = true THEN latest.elo_rating
+        ELSE orig.elo_rating
     END AS elo_rating,
-    orig.elo_rating::int AS original_rating,
+    orig.elo_rating AS original_rating,
     orig.win_total
 FROM {{ ref( 'prep_team_ratings' ) }} orig
-LEFT JOIN {{ ref( 'prep_latest_ratings' )}} latest ON latest.team = orig.team
+LEFT JOIN {{ ref( 'prep_elo_post' ) }} latest ON latest.team = orig.team
 GROUP BY ALL
