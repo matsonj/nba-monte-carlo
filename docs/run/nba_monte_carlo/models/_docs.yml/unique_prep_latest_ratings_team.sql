b462b871604b105dcf6768d4d2f833cb8b9676a7
Jacob Matson <matsonj@gmail.com>
2022-10-23 01:46:29 +0000

    one more doc update

---
+++ docs/run/nba_monte_carlo/models/_docs.yml/unique_prep_latest_ratings_team.sql
@@ -0,0 +1,22 @@
+select
+      count(*) as failures,
+      count(*) != 0 as should_warn,
+      count(*) != 0 as should_error
+    from (
+      
+    
+    
+
+select
+    team as unique_field,
+    count(*) as n_records
+
+from "main"."main_prep"."prep_latest_ratings"
+where team is not null
+group by team
+having count(*) > 1
+
+
+
+      
+    ) dbt_internal_test
