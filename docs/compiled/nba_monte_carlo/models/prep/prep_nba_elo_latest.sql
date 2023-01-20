4b012b3e838be982d79ff6446b74eebfad971423
Jacob Matson <matsonj@gmail.com>
2023-01-20 18:31:43 +0000

    doc updates

--- docs/compiled/nba_monte_carlo/models/prep/prep_nba_elo_latest.sql
+++ docs/compiled/nba_monte_carlo/models/prep/prep_nba_elo_latest.sql
@@ -1,4 +1,3 @@
 SELECT *
 FROM '/workspaces/nba-monte-carlo/data/data_catalog/psa/nba_elo_latest/*.parquet'
-QUALIFY
-    ROW_NUMBER() OVER (PARTITION BY _smart_source_lineno ORDER BY _sdc_extracted_at DESC) = 1
+GROUP BY ALL
