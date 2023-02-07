5c8d4080b594f2be327dc4b70925704db2958390
Jacob Matson <matsonj@gmail.com>
2023-02-07 21:04:23 +0000

    update docs etc

---
+++ transform/models/conformed/reg_season/reg_season_predictions.sql
@@ -0,0 +1,15 @@
+SELECT 
+    game_id,
+    home_team,
+    home_team_elo_rating,
+    visiting_team,
+    visiting_team_elo_rating,
+    home_team_win_probability,
+    winning_team,
+    include_actuals,
+    COUNT(*) AS occurances
+FROM {{ ref( 'reg_season_simulator' ) }} S
+    GROUP BY ALL
+
+
+
