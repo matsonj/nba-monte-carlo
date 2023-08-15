a180c32241df43c97f6defe8136fba5d5f194a61
Jacob Matson <matsonj@gmail.com>
2023-08-15 05:29:22 +0000

    the season is simulating!

---
+++ transform/models/nfl/conformed/reg_season/nfl_reg_season_simulator.sql
@@ -0,0 +1,16 @@
+SELECT 
+    R.scenario_id,
+    S.*,
+    {{ elo_calc( 'S.home_team_elo_rating', 'S.visiting_team_elo_rating' ) }} as home_team_win_probability,
+    R.rand_result,
+    CASE 
+        -- WHEN LR.include_actuals = true THEN LR.winning_team
+        WHEN {{ elo_calc( 'S.home_team_elo_rating', 'S.visiting_team_elo_rating' ) }}  >= R.rand_result THEN S.home_team
+        ELSE S.visiting_team
+    END AS winning_team,
+    --COALESCE(LR.include_actuals, false) AS include_actuals
+    false as include_actuals
+FROM {{ ref( 'nfl_schedules' ) }} S
+LEFT JOIN {{ ref( 'nfl_random_num_gen' ) }} R ON R.game_id = S.game_id
+-- LEFT JOIN 'nfl_latest_results'  LR ON LR.game_id = S.game_id
+WHERE S.type = 'reg_season'
