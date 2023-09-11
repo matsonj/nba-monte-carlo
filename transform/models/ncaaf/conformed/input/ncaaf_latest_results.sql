6e8b5515f123238b6dde0618bdb21e75aa1a8e39
Jacob Matson <matsonj@gmail.com>
2023-09-11 05:08:58 +0000

    add elo rfwd for ncaa

--- transform/models/ncaaf/conformed/input/ncaaf_latest_results.sql
+++ transform/models/ncaaf/conformed/input/ncaaf_latest_results.sql
@@ -1,25 +1,30 @@
-SELECT
-    S.game_id,
-    S.Week_number,
-    S.home_team, 
-    CASE
-        WHEN S.home_team = R.Winner THEN R.Winner_Pts
-        ELSE R.Loser_Pts 
-    END AS home_team_score,
-    S.visiting_team AS visiting_team,
-    CASE
-        WHEN S.visiting_team = R.Winner THEN R.Winner_Pts
-        ELSE R.Loser_Pts 
-    END AS  visiting_team_score,
-    R.Winner AS winning_team,
-    R.Loser AS losing_team,
-    {{ var('include_actuals') }} AS include_actuals,
+with cte_inner as (
+    SELECT
+        S.game_id,
+        S.Week_number,
+        S.home_team, 
+        CASE
+            WHEN S.home_team = R.Winner THEN R.Winner_Pts
+            ELSE R.Loser_Pts 
+        END AS home_team_score,
+        S.visiting_team AS visiting_team,
+        CASE
+            WHEN S.visiting_team = R.Winner THEN R.Winner_Pts
+            ELSE R.Loser_Pts 
+        END AS  visiting_team_score,
+        R.Winner AS winning_team,
+        R.Loser AS losing_team,
+        {{ var('include_actuals') }} AS include_actuals,
+
+    FROM {{ ref( 'ncaaf_schedules' ) }} S
+        LEFT JOIN {{ ref( 'ncaaf_prep_results' ) }} R ON R.Wk = S.Week_number
+            AND (S.visiting_team = R.Winner OR S.visiting_team = R.Loser)
+    WHERE home_team_score IS NOT NULL 
+    GROUP BY ALL
+)
+SELECT *,
     CASE
         WHEN visiting_team_score > home_team_score THEN 1
         ELSE 0
     END AS game_result
-FROM {{ ref( 'ncaaf_schedules' ) }} S
-    LEFT JOIN {{ ref( 'ncaaf_prep_results' ) }} R ON R.Wk = S.Week_number
-        AND (S.visiting_team = R.Winner OR S.visiting_team = R.Loser)
-WHERE home_team_score IS NOT NULL 
-GROUP BY ALL
+FROM cte_inner
