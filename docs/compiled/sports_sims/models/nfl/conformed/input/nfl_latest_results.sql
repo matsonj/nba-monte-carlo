e6b1b91ba2723b720261ba3b302ff217b9b2eb0f
Jacob Matson <matsonj@gmail.com>
2023-09-19 05:58:13 +0000

    docs update

---
+++ docs/compiled/sports_sims/models/nfl/conformed/input/nfl_latest_results.sql
@@ -0,0 +1,31 @@
+with cte_inner as (
+    SELECT
+        S.id as game_id,
+        S.week as week_number,
+        S.HomeTm AS home_team, 
+        CASE
+            WHEN S.HomeTm = R.Winner THEN R.Winner_Pts
+            ELSE R.Loser_Pts 
+        END AS home_team_score,
+        S.VisTm AS visiting_team,
+        CASE
+            WHEN S.VisTm = R.Winner THEN R.Winner_Pts
+            ELSE R.Loser_Pts 
+        END AS  visiting_team_score,
+        R.Winner AS winning_team,
+        R.Loser AS losing_team,
+        True AS include_actuals,
+
+    FROM "mdsbox"."main"."nfl_prep_schedule" S
+        LEFT JOIN "mdsbox"."main"."nfl_prep_results" R ON R.Wk = S.week
+            AND (S.VisTm = R.Winner OR S.VisTm = R.Loser)
+    WHERE home_team_score IS NOT NULL 
+    GROUP BY ALL
+)
+SELECT *,
+    CASE
+        WHEN visiting_team_score > home_team_score THEN 1
+        WHEN visiting_team_score = home_team_score THEN 0.5
+        ELSE 0
+    END AS game_result
+FROM cte_inner
--- docs/compiled/sports_sims/models/nfl/conformed/reg_season/nfl_reg_season_actuals.sql
+++ docs/compiled/sports_sims/models/nfl/conformed/reg_season/nfl_reg_season_actuals.sql
@@ -2,7 +2,7 @@ WITH cte_wins AS (
     SELECT 
         winning_team,
         COUNT(*) as wins
-    FROM "mdsbox"."main"."latest_results"
+    FROM "mdsbox"."main"."nfl_latest_results"
     GROUP BY ALL
 ),
 
@@ -10,14 +10,14 @@ cte_losses AS (
     SELECT 
         losing_team,
         COUNT(*) as losses
-    FROM "mdsbox"."main"."latest_results"
+    FROM "mdsbox"."main"."nfl_latest_results"
     GROUP BY ALL
 )
 
 SELECT
-    T.team,
+    T.team_long as team,
     COALESCE(W.wins, 0) AS wins,
     COALESCE(L.losses, 0) AS losses
-FROM "mdsbox"."main"."teams" T
-LEFT JOIN cte_wins W ON W.winning_team = T.team
-LEFT JOIN cte_losses L ON L.losing_team = T.Team
+FROM "mdsbox"."main"."nfl_teams" T
+LEFT JOIN cte_wins W ON W.winning_team = T.team_long
+LEFT JOIN cte_losses L ON L.losing_team = T.Team_long
