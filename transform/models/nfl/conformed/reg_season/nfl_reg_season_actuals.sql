6312d65e2e1f4c11553d6e47b58ec47451dc5fb7
Jacob Matson <matsonj@gmail.com>
2023-09-19 05:13:34 +0000

    adding nfl models

---
+++ transform/models/nfl/conformed/reg_season/nfl_reg_season_actuals.sql
@@ -0,0 +1,23 @@
+WITH cte_wins AS (
+    SELECT 
+        winning_team,
+        COUNT(*) as wins
+    FROM {{ ref( 'nfl_latest_results' ) }}
+    GROUP BY ALL
+),
+
+cte_losses AS (
+    SELECT 
+        losing_team,
+        COUNT(*) as losses
+    FROM {{ ref( 'nfl_latest_results' ) }}
+    GROUP BY ALL
+)
+
+SELECT
+    T.team_long as team,
+    COALESCE(W.wins, 0) AS wins,
+    COALESCE(L.losses, 0) AS losses
+FROM {{ ref( 'nfl_teams' ) }} T
+LEFT JOIN cte_wins W ON W.winning_team = T.team_long
+LEFT JOIN cte_losses L ON L.losing_team = T.Team_long
