94b26402931a9046897c100da6f1bba708421962
Jacob Matson <matsonj@gmail.com>
2023-09-20 05:12:11 +0000

    trying stuff

--- evidence/sources/nfl/future_games.sql
+++ evidence/sources/nfl/future_games.sql
@@ -9,8 +9,8 @@ SELECT
     home_team_win_probability/10000 AS home_win_pct1,
     american_odds AS odds,
     CASE
-        WHEN home_win_pct1 >= 0.52 THEN ROUND( -27.117 * home_win_pct1 + 13.512, 1 )
-        ELSE ROUND( -27.275 * home_win_pct1 + 14.759, 1 ) 
+        WHEN home_win_pct1 >= 0.52 THEN ROUND( -55.048 * home_win_pct1^2 + 47.837 * home_win_pct1 - 11.56, 1 )
+        ELSE ROUND( 55.564 * home_win_pct1^2 -67.229 * home_win_pct1 + 21.501, 1 )
     END AS implied_line_num1
 FROM nfl_reg_season_predictions
 WHERE include_actuals = false AND winning_team = home_team
