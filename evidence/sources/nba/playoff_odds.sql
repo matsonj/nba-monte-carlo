a54e2c0d79779d2bac2e8f2b5936146af4ebef8a
Jacob Matson <matsonj@gmail.com>
2023-11-07 23:28:06 -0800

    fix query ref

--- evidence/sources/nba/playoff_odds.sql
+++ evidence/sources/nba/playoff_odds.sql
@@ -3,5 +3,5 @@ SELECT
     COALESCE(SUM(odds_pct1) FILTER (WHERE season_result = 'top six seed'),0) as top_six_pct1,
     COALESCE(SUM(odds_pct1) FILTER (WHERE season_result = 'play in'),0) as play_in_pct1,
     COALESCE(SUM(odds_pct1) FILTER (WHERE season_result = 'missed playoffs'),0) as missed_playoffs_pct1
-FROM ${playoff_odds_by_team_by_wins}
+FROM ${playoff_wins}
 GROUP BY ALL
