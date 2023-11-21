f1c6f53e20bca8a5decc29a65330ee9c1e5831c3
Jacob Matson <matsonj@gmail.com>
2023-11-21 09:04:51 +0000

    add nba game drilldown

--- evidence/sources/nba/future_games.sql
+++ evidence/sources/nba/future_games.sql
@@ -1,10 +1,13 @@
 SELECT
     game_id,
+    date,
     CASE WHEN type = 'tournament' THEN '🏆' ELSE '' END AS "T",
     visiting_team as visitor,
     visiting_team_elo_rating AS visitor_ELO,
     home_team as home, 
     home_team_elo_rating AS home_ELO,
+    home_team_elo_rating - visiting_team_elo_rating AS elo_diff,
+    elo_diff + 70 AS elo_diff_hfa,
     home_team_win_probability/10000 AS home_win_pct1,
     american_odds,
     implied_line AS implied_line_num1,
