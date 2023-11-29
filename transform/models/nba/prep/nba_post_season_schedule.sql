552352c63b20aa0bc99fb5d7585ca448b1310a98
Jacob Matson <matsonj@gmail.com>
2023-11-29 23:28:30 +0000

    fix post season

--- transform/models/nba/prep/nba_post_season_schedule.sql
+++ transform/models/nba/prep/nba_post_season_schedule.sql
@@ -1,7 +1,7 @@
 SELECT
     S.id::int AS game_id,
-    S.type,
     S.date,
+    S.type,
     S.series_id,
     NULL AS visiting_conf,
     S.VisTm AS visiting_team,
@@ -10,8 +10,8 @@ SELECT
     S.HomeTm AS home_team,
     NULL AS home_team_elo_rating
 FROM {{ ref( 'nba_raw_schedule' ) }} AS S
-LEFT JOIN {{ ref( 'nba_ratings' ) }} V ON V.team = S.VisTm
-LEFT JOIN {{ ref( 'nba_ratings' ) }} H ON H.team = S.HomeTm
-LEFT JOIN {{ ref( 'nba_elo_rollforward' ) }} R ON R.game_id = S.id
+--LEFT JOIN {{ ref( 'nba_ratings' ) }} V ON V.team = S.VisTm
+--LEFT JOIN {{ ref( 'nba_ratings' ) }} H ON H.team = S.HomeTm
+--LEFT JOIN {{ ref( 'nba_elo_rollforward' ) }} R ON R.game_id = S.id
 WHERE S.type <> 'reg_season'
 GROUP BY ALL
