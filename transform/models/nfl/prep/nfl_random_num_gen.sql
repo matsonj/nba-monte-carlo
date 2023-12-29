1c8c24a3366b982a6281d9c72733dc17ad8aa9d9
Jacob Matson <matsonj@gmail.com>
2023-12-29 01:01:04 +0000

    build fix

--- transform/models/nfl/prep/nfl_random_num_gen.sql
+++ transform/models/nfl/prep/nfl_random_num_gen.sql
@@ -14,5 +14,5 @@ SELECT
     {{ var( 'sim_start_game_id' ) }} AS sim_start_game_id
 FROM cte_scenario_gen AS i
 CROSS JOIN {{ ref( 'nfl_schedules' ) }} AS S
-LEFT JOIN {{ ref( 'nfl_latest_results' )}} AS R ON R.game_id = S.game_id
-WHERE R.game_id IS NULL OR (R.game_id IS NOT NULL AND i.scenario_id = 1)
+--LEFT JOIN {{ ref( 'nfl_latest_results' )}} AS R ON R.game_id = S.game_id
+--WHERE R.game_id IS NULL OR (R.game_id IS NOT NULL AND i.scenario_id = 1)
