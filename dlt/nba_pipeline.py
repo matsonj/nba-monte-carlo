2f1cfe183695f988f67afdb475670d68ccfbc356
Jacob Matson <matsonj@gmail.com>
2024-10-24 01:04:28 +0000

    bump api for new season

--- dlt/nba_pipeline.py
+++ dlt/nba_pipeline.py
@@ -2,7 +2,7 @@ import dlt
 from dlt.sources.helpers import requests
 
 # Specify the URL of the API endpoint
-url = "https://api.pbpstats.com/get-games/nba?Season=2023-24&SeasonType=Regular%20Season"
+url = "https://api.pbpstats.com/get-games/nba?Season=2024-25&SeasonType=Regular%20Season"
 # Make a request and check if it was successful
 response = requests.get(url)
 response.raise_for_status()
