a0ec96ff4ed285bda5953a925bbfa713672b1579
Jacob Matson <matsonj@gmail.com>
2024-10-27 17:36:40 +0000

    fix playoffs filter

--- transform/models/nba/raw/nba_raw_schedule.sql
+++ transform/models/nba/raw/nba_raw_schedule.sql
@@ -10,4 +10,5 @@ select
     notes,
     series_id
 from {{ source("nba", "nba_schedule") }}
-where arena <> 'Placeholder' -- removing IST games w/o teams & arena
+where arena is null -- make sure playoffs are included
+    or arena <> 'Placeholder' -- removing IST games w/o teams & arena
