0ccc539c7ee18648125dfe7f8701ff7ba1ab6d1d
Jacob Matson <matsonj@gmail.com>
2024-12-14 02:57:46 +0000

    minor layout tweaks

---
+++ evidence/pages/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '@evidence-dev/tailwind/fonts.css';
+	import './app.css';
+	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	export let data;
+</script>
+
+<EvidenceDefaultLayout {data} 
+	xProfile="https://www.x.com/matsonj"
+	blueskyProfile="https://bsky.app/profile/matsonj.com"
+	githubRepo="https://www.github.com/matsonj/nba-monte-carlo"
+	title="mdsinabox"
+	builtWithEvidence=true>
+	<slot slot="content" />
+</EvidenceDefaultLayout>
