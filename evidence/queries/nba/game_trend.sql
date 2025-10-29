ac74826ba30fea0eaf22258fa04cc8c4ba161275
Jacob Matson <matsonj@gmail.com>
2025-10-29 11:20:52 -0700

    2024 -> 2025

--- evidence/queries/nba/game_trend.sql
+++ evidence/queries/nba/game_trend.sql
@@ -8,11 +8,11 @@ UNION ALL
 SELECT game_id, hmtm as team, home_team_elo_rating as elo_rating, elo_change*-1 as elo_change
 FROM src_nba_results_log )
 SELECT 
-    COALESCE(AR.game_date,'2024-10-21')::date AS date,
+    COALESCE(AR.game_date,'2025-10-20')::date AS date,
     RL.team, 
     RL.elo_rating, 
     RL.elo_change,
-    sum(RL.elo_change) over (partition by team order by COALESCE(AR.game_date,'2024-10-21') asc rows between unbounded preceding and current row) as cumulative_elo_change_num0,
+    sum(RL.elo_change) over (partition by team order by COALESCE(AR.game_date,'2025-10-20') asc rows between unbounded preceding and current row) as cumulative_elo_change_num0,
     RL.elo_rating + RL.elo_change AS elo_post
 FROM cte_games RL
 LEFT JOIN src_nba_results_log AR ON AR.game_id = RL.game_id
